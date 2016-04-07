c014798179e3b2dc8be44f542a37c0afdf2ffb30
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:17:14 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import test from 'ava';
 import m from './';
 
 test(t => {
-	t.same(m([1, 2, 3, 4], {count: 1}).length, 1);
-	t.same(m([1, 2, 3, 4], {count: 3}).length, 3);
-	t.notSame(m([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], {count: 5}), [1, 2, 3, 4, 5]);
+	t.deepEqual(m([1, 2, 3, 4], {count: 1}).length, 1);
+	t.deepEqual(m([1, 2, 3, 4], {count: 3}).length, 3);
+	t.notDeepEqual(m([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], {count: 5}), [1, 2, 3, 4, 5]);
 	t.notThrows(() => m([1]));
-	t.same(m([1]).length, 1);
+	t.deepEqual(m([1]).length, 1);
 	t.throws(() => m('1'), 'Expected an array as the first argument');
 	t.throws(() => m([1, 2, 3, 4], {count: 5}), 'Count must be lower or the same as the number of picks');
 });
