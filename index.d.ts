72780dc1fa661a623f20e1237195a1f6dcf52b24
rigwild <me@rigwild.dev>
2021-03-20 13:18:55 +0000

    Fit type styleguide

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,28 @@
-/**
- * Pick random items from a list
- * 
- * @example
- * ```
- * import pickRandom from 'pick-random'
- * 
- * pickRandom(['Sindre', 'Stephen', 'Unicorn', 'Pascal', 'Addy', 'Pony']);
- * // => ['Unicorn']
- * 
- * pickRandom(['Sindre', 'Stephen', 'Unicorn', 'Pascal', 'Addy', 'Pony'], {count: 2});
- * // => ['Unicorn', 'Pony']
- * ```
- */
-declare const pickRandom: <T>(data: T[], opts?: { count?: number }) => T[]
+declare namespace pickRandom {
+	interface Options {
+        /**
+        Amount of elements to pick.
 
-export = pickRandom 
+        @default 1
+        */
+        count?: number;
+    }
+}
+
+/**
+Pick random items from a list
+
+@example
+```
+import pickRandom from 'pick-random'
+
+pickRandom(['Sindre', 'Stephen', 'Unicorn', 'Pascal', 'Addy', 'Pony']);
+//=> ['Unicorn']
+
+pickRandom(['Sindre', 'Stephen', 'Unicorn', 'Pascal', 'Addy', 'Pony'], {count: 2});
+//=> ['Unicorn', 'Pony']
+```
+*/
+declare function pickRandom<T>(data: T[], options?: pickRandom.Options): T[];
+
+export = pickRandom;
---
+++ index.test-d.ts
@@ -0,0 +1,9 @@
+import {expectType,expectError} from 'tsd';
+import pickRandom = require('.');
+
+expectType<string[]>(pickRandom(['foo']));
+expectType<string[]>(pickRandom(['foo', 'bar']));
+expectType<number[]>(pickRandom([1, 2, 3]));
+expectType<number[]>(pickRandom([1, 2, 3], {count: 3}));
+expectType<string[][]>(pickRandom([['foo'], ['bar']], {count: 2}));
+expectError(pickRandom(1));
